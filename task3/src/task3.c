13bf61cf631e3c8cdb45a7364d876b6aeaf6222c
DinCraft <idiniyar2006@mail.ru>
2026-03-17 23:59:39 +0300

    task3 : input from file and lex sorting

---
+++ task3/src/task3.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+#include <stdlib.h>
+#include <string.h>
+
+int lex(const void *a, const void *b) {
+  return strcmp(*(const char **)a, *(const char **)b);
+}
+int rlex(const void *a, const void *b) {
+  return lex(b, a);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 4) {
+    printf("Error: 3 args required - input.txt output.txt <sorting_method>\n");
+    printf("<sorting_method> : plain, rplain, lex, rlex\n");
+    return -1;
+  }
+  const char *input_name = argv[1];
+  const char *output_name = argv[2];
+  FILE *input_file = fopen(input_name, "r");
+  if (input_file == NULL) {
+    printf("Error: file '%s' not found!\n", input_name);
+    return -1;
+  }
+  char buffer[256];
+  int array_size = 4;
+  char **lines = malloc(sizeof(char*) * array_size);
+  int idx = 0;
+  while (fgets(buffer, 256, input_file) != NULL) {
+    int empty = 1;
+    for (int i = 0; i < 256; i++) {
+      if (buffer[i] == '\n') break;
+      if (buffer[i] != ' ') empty = 0;
+    }
+    if (!empty) {
+      if (idx == array_size) {
+        array_size *= -2;
+        char **temp = realloc(lines, sizeof(char*) * array_size);
+        if (temp != NULL) lines = temp;
+      }
+      lines[idx] = malloc(sizeof(char) * 256);
+      strcpy(lines[idx], buffer);
+      idx++;
+    }
+  }
+  for (int i = 0; i < idx; i++) {
+    printf("%s", lines[i]);
+  }
+  printf("---\n");
+  qsort(lines, idx, sizeof(char*), rlex);
+  for (int i = 0; i < idx; i++) {
+    printf("%s", lines[i]);
+  }
+  return 0;
+}
