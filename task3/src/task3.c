2e8db84317a70416d69bcdbef18aa78447a6acf3
DinCraft <idiniyar2006@mail.ru>
2026-03-18 08:11:55 +0300

    output done

--- task3/src/task3.c
+++ task3/src/task3.c
@@ -32,6 +32,8 @@ int rplain(const void *a, const void *b) {
 
 char **input(FILE *input_file, int *lines_amount);
 
+void output(char **lines, int *lines_amount, const char *ouput_name);
+
 int main(int argc, char *argv[])
 {
   if (argc != 4) {
@@ -42,33 +44,43 @@ int main(int argc, char *argv[])
   const char *input_name = argv[1];
   const char *output_name = argv[2];
   FILE *input_file = fopen(input_name, "r");
-  int lines_amount;
-  char **lines = input(input_file, &lines_amount);
   if (input_file == NULL) {
     printf("Error: file '%s' not found!\n", input_name);
     return -1;
   }
+  int lines_amount;
+  char **lines = input(input_file, &lines_amount);
+  fclose(input_file);
   qsort(lines, lines_amount, sizeof(char*), plain);
-  for (int i = 0; i < lines_amount; i++) {
-    printf("%s", lines[i]);
-  }
+  output(lines, &lines_amount, output_name);
   return 0;
+}
+
+void output(char **lines, int *lines_amount, const char *ouput_name) {
+  FILE *output_file = fopen(ouput_name, "w");
+  if (output_file == NULL) return;
+  for (int i = 0; i < *lines_amount; i++) {
+    fputs(lines[i], output_file);
+  }
+  fclose(output_file);
 }
 
 char **input(FILE *input_file, int *lines_amount) {
   char buffer[256];
   int array_size = 4;
   char **lines = malloc(sizeof(char*) * array_size);
+  if (lines == NULL) return NULL;
   int idx = 0;
   while (fgets(buffer, 256, input_file) != NULL) {
     int empty = 1;
+    // check if empty
     for (int i = 0; i < 256; i++) {
       if (buffer[i] == '\n') break;
       if (buffer[i] != ' ') empty = 0;
     }
     if (!empty) {
       if (idx == array_size) {
-        array_size *= -2;
+        array_size *= 2;
         char **temp = realloc(lines, sizeof(char*) * array_size);
         if (temp != NULL) lines = temp;
       }
