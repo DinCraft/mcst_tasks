a94d703306450a687100bfca8501b14b8dfbfaab
DinCraft <idiniyar2006@mail.ru>
2026-03-23 22:49:07 +0300

    thread func

--- task7/src/task7.c
+++ task7/src/task7.c
@@ -7,6 +7,8 @@
 
 char **input(FILE *input_file, int *lines_amount);
 
+int thread_function(pid_t *p, const char *input_name);
+
 int main(int argc, char *argv[])
 {
   if (argc != 2) {
@@ -19,39 +21,50 @@ int main(int argc, char *argv[])
     perror("fork fail");
     exit(1);
   }
-  else if ( p == 0) {
-    FILE *input_file = fopen(argv[1], "r");
-    if (input_file == NULL) {
-      printf("Error: file '%s' not found!\n", argv[1]);
-      return -1;
-    } 
-    int lines_amount = 0;
-    char **lines = input(input_file, &lines_amount);
-    fclose(input_file);
+  else {
+    thread_function(&p, argv[1]);
+  }
 
-    FILE *output_file = fopen("child_copy", "w");
-    for (int i = 0; i < lines_amount; i++) {
-      fputs(lines[i], output_file);
-    }
-    fclose(output_file);
+  return 0;
+}
+
+int thread_function(pid_t *p, const char *input_name) {
+  FILE *input_file = fopen(input_name, "r");
+  if (input_file == NULL) {
+    printf("Error: file '%s' not found!\n", input_name);
+    return -1;
+  } 
+  int lines_amount = 0;
+  char **lines = input(input_file, &lines_amount);
+  fclose(input_file);
+
+  const char *output_name = "parent_copy";
+  if (*p == 0) {
+    output_name = "child_copy";
+  }
+  FILE *output_file = fopen(output_name, "w");
+  for (int i = 0; i < lines_amount; i++) {
+    fputs(lines[i], output_file);
+  }
+  fclose(output_file);
+
+  input_file = fopen(output_name, "r");
+  if (input_file == NULL) {
+    printf("Error: file '%s' not found!\n", input_name);
+    return -1;
+  } 
+  lines_amount = 0;
+  lines = input(input_file, &lines_amount);
+  if (*p == 0) {
+    printf("Child:\n");
   }
   else {
-    FILE *input_file = fopen(argv[1], "r");
-    if (input_file == NULL) {
-      printf("Error: file '%s' not found!\n", argv[1]);
-      return -1;
-    } 
-    int lines_amount = 0;
-    char **lines = input(input_file, &lines_amount);
-    fclose(input_file);
-
-    FILE *output_file = fopen("parent_copy", "w");
-    for (int i = 0; i < lines_amount; i++) {
-      fputs(lines[i], output_file);
-    }
-    fclose(output_file);
+    printf("Parent:\n");
   }
-
+  for (int i = 0; i < lines_amount; i++) {
+    printf("%s", lines[i]);
+  }
+  fclose(input_file);
   return 0;
 }
 
