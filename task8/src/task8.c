288d8b5d27b5989508812ae3c4ea83fa2182995f
DinCraft <idiniyar2006@mail.ru>
2026-03-24 21:50:09 +0300

    signals handling

--- task8/src/task8.c
+++ task8/src/task8.c
@@ -1,12 +1,16 @@
-#include "stdio.h"
-#include "getopt.h"
-#include <bits/getopt_ext.h>
+#include <stdio.h>
+#include <getopt.h>
+#include <signal.h>
 
 static struct option long_options[] = {
   {"signal",    no_argument, 0,  0},
   {"sigaction", no_argument, 0,  0},
   {0,           0,           0,  0}
 };
+
+void signal_handler(int sigid);
+
+void sigaction_handler(int sigid);
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -18,13 +22,30 @@ int main(int argc, char *argv[]) {
   if (c == 0) {
     if (option_index == 0) {
       printf("signal\n");
+      signal(SIGINT, signal_handler);
     }
     else if (option_index == 1) {
       printf("sigaction\n");
+      struct sigaction sa;
+      sa.sa_handler = sigaction_handler;
+      sigaction(SIGINT, &sa, NULL);
     }
   }
   else {
     printf("idk\n");
   }
+  while (1) {}
   return 0;
+}
+
+void signal_handler(int sigid) {
+  printf("sigint\n");
+  signal(SIGINT, NULL);
+}
+
+void sigaction_handler(int sigid) {
+  printf("sigint\n ");
+  struct sigaction sa;
+  sa.sa_handler = SIG_DFL;
+  sigaction(SIGINT, &sa, NULL);
 }
