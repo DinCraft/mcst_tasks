095eb2e857519da72e79986b4eccde368ef89322
DinCraft <idiniyar2006@mail.ru>
2026-03-31 05:07:38 +0300

    task7: splitting thread func

--- task7/src/task7.c
+++ task7/src/task7.c
@@ -7,6 +7,8 @@
 
 char **input(FILE *input_file, int *lines_amount);
 
+void copy(const char *input_name, const char *output_name);
+
 int thread_function(pid_t *p, const char *input_name);
 
 int main(int argc, char *argv[])
@@ -28,43 +30,30 @@ int main(int argc, char *argv[])
   return 0;
 }
 
-int thread_function(pid_t *p, const char *input_name) {
+void copy(const char *input_name, const char *output_name) {
   FILE *input_file = fopen(input_name, "r");
   if (input_file == NULL) {
     printf("Error: file '%s' not found!\n", input_name);
-    return -1;
+    return;
   } 
   int lines_amount = 0;
   char **lines = input(input_file, &lines_amount);
   fclose(input_file);
 
-  const char *output_name = "parent_copy";
-  if (*p == 0) {
-    output_name = "child_copy";
-  }
   FILE *output_file = fopen(output_name, "w");
   for (int i = 0; i < lines_amount; i++) {
     fputs(lines[i], output_file);
   }
   fclose(output_file);
+}
 
-  input_file = fopen(output_name, "r");
-  if (input_file == NULL) {
-    printf("Error: file '%s' not found!\n", input_name);
-    return -1;
-  } 
-  lines_amount = 0;
-  lines = input(input_file, &lines_amount);
+int thread_function(pid_t *p, const char *input_name) {
   if (*p == 0) {
-    printf("Child:\n");
+    copy(input_name, "child_copy");
   }
   else {
-    printf("Parent:\n");
+    copy(input_name, "parent_copy");
   }
-  for (int i = 0; i < lines_amount; i++) {
-    printf("%s", lines[i]);
-  }
-  fclose(input_file);
   return 0;
 }
 
